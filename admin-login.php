8707d6a8fe8ec29e0f5492318618c5fdd895f035
Benjamin Venema <58939955+bvsvenema@users.noreply.github.com>
2023-06-20 13:21:14 +0200

    bonk

---
+++ admin-login.php
@@ -0,0 +1,37 @@
+<?php
+// Now we check if the data from the login form was submitted, isset() will check if the data exists.
+//if ( !isset($_POST['username'], $_POST['password']) ) {?>
+<html>
+<head>
+		<meta charset="utf-8">
+		<title>Login</title>
+        <link href="assets/css/style.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+	</head>
+	<body>
+		<div class="login">
+			<h1>Login</h1>
+			<form action="authenticate.php" method="post">
+				<label for="username">
+					<i class="fas fa-user"></i>
+				</label>
+				<input type="text" name="username" placeholder="Username" id="username" required>
+				<label for="password">
+					<i class="fas fa-lock"></i>
+				</label>
+				<input type="password" name="password" placeholder="Password" id="password" required>
+				<input type="submit" value="Login">
+			</form>
+		</div>
+	</body>
+</html>
+<?php	
+//}else{
+?>
+<html>
+    <head>
+        Bonk
+    </head>
+
+</html>
+<?php //} ?>
